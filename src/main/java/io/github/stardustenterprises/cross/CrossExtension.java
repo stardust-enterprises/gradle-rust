c2c462e617cca2603c2af86c17caee3787db201b
xTrM-EN <xtram.dev@gmail.com>
2021-10-22 20:39:09 +0200

    Update package name for Gradle Plugin Portal

---
+++ src/main/java/io/github/stardustenterprises/cross/CrossExtension.java
@@ -0,0 +1,74 @@
+// Copyright 2021 Arc'blroth
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.github.stardustenterprises.cross;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Configuration options for the Cross wrapper plugin.
+ */
+public class CrossExtension {
+
+    /**
+     * Location of the <code>cross</code> executable.
+     * By default, the plugin will use the Cross
+     * executable located on the path.
+     */
+    public String cargoCommand = "cross";
+
+    /**
+     * The rust toolchain to use.
+     * By default, the plugin will use the default toolchain as
+     * <a href="https://rust-lang.github.io/rustup/overrides.html">
+     * resolved by rustup
+     * </a>.
+     */
+    public String toolchain = null;
+
+    /**
+     * Location of the crate to build.
+     * By default, this is the current project directory.
+     */
+    public String crate = null;
+
+    /**
+     * List of the locations of the outputs executables/libraries.
+     * Each entry in this list is a pair between a target triple
+     * and the name of the output file. Use <code>""</code>
+     * (the empty string) to specify the default target.
+     */
+    public Map<String, String> outputs = new ConcurrentHashMap<>();
+
+    /**
+     * Build profile to use. If this is not set to <code>"debug"</code>,
+     * the plugin assumes a release profile.
+     * By default, this is set to the <code>"release"</code> release profile.
+     */
+    public String profile = "release";
+
+    /**
+     * Additional arguments to pass to Cross.
+     */
+    public List<String> arguments = new ArrayList<>();
+
+    /**
+     * Additional environmental variables to set while
+     * launching Cross.
+     */
+    public Map<String, String> environment = new ConcurrentHashMap<>();
+}
