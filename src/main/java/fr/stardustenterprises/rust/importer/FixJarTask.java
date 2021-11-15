7e8d48f3cf426b0bce337a34a3efe096b00f4282
xTrM-EN <26600206+xtrm-en@users.noreply.github.com>
2021-11-15 21:13:37 +0100

    YESSSSSSSSSSSS

--- src/main/java/fr/stardustenterprises/rust/importer/FixJarTask.java
+++ src/main/java/fr/stardustenterprises/rust/importer/FixJarTask.java
@@ -1,7 +1,6 @@
 package fr.stardustenterprises.rust.importer;
 
 import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
 import org.apache.commons.io.FileUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
@@ -9,29 +8,60 @@ import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class FixJarTask extends DefaultTask {
 
     @TaskAction
-    public void fixJar() {
+    public void fixJar() throws IOException {
         AbstractArchiveTask jarTask = (AbstractArchiveTask) getProject().getTasks().getByName("jar");
         File jarFile = jarTask.getArchiveFile().get().getAsFile();
 
         ZipFile zipFile = new ZipFile(jarFile);
         File cacheDir = new File(getProject().getBuildDir(), "rustImport");
-        if(cacheDir.exists()) {
-            try {
-                FileUtils.deleteDirectory(cacheDir);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (cacheDir.exists()) {
+            FileUtils.deleteDirectory(cacheDir);
         }
         cacheDir.mkdirs();
 
-        try {
-            zipFile.extractAll(cacheDir.getAbsolutePath());
-        } catch (ZipException e) {
-            e.printStackTrace();
+        File javaExtract = new File(cacheDir, "java");
+        javaExtract.mkdir();
+
+        zipFile.extractAll(javaExtract.getAbsolutePath());
+
+        File exportZip = new File(javaExtract, "export.zip");
+        if (!exportZip.exists()) {
+            throw new RuntimeException("Export zip doesn't exist!");
+        }
+
+        File nativeExtract = new File(cacheDir, "rust");
+        nativeExtract.mkdir();
+
+        ZipFile exportZipFile = new ZipFile(exportZip);
+        exportZipFile.extractAll(nativeExtract.getAbsolutePath());
+
+        exportZip.delete();
+
+        File[] files = nativeExtract.listFiles();
+        assert files != null;
+
+        for (File file : files) {
+            File dest = new File(javaExtract, file.getName());
+            if (file.isDirectory()) {
+                FileUtils.copyDirectory(file, dest);
+            } else {
+                FileUtils.copyFile(file, dest);
+            }
+        }
+
+        jarFile.delete();
+        ZipFile finalZip = new ZipFile(jarFile);
+        for (File f : Objects.requireNonNull(javaExtract.listFiles())) {
+            if (f.isFile()) {
+                finalZip.addFile(f);
+            } else {
+                finalZip.addFolder(f);
+            }
         }
     }
 }
