8b4e8f72e3d30a586e6acac79d7a850c5e597cea
xTrM-EN <xtram.dev@gmail.com>
2021-12-14 19:45:21 +0100

    no do not publish that that is terrible no no no no no no

--- example_app/target/release/deps/example_app-3f774cbd9ede20cc.d
+++
@@ -1,5 +0,0 @@
-/home/xtrm/IdeaProjects/gradle-cargo-wrapper/example_app/target/release/deps/example_app-3f774cbd9ede20cc: src/main.rs
-
-/home/xtrm/IdeaProjects/gradle-cargo-wrapper/example_app/target/release/deps/example_app-3f774cbd9ede20cc.d: src/main.rs
-
-src/main.rs:
--- example_app/target/release/example_app.d
+++
@@ -1,1 +0,0 @@
-/home/xtrm/IdeaProjects/gradle-cargo-wrapper/example_app/target/release/example_app: /home/xtrm/IdeaProjects/gradle-cargo-wrapper/example_app/src/main.rs
