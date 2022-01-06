4b5f06a8bca51868d3817eba791b225866606f7d
xtrm-en <xtram.dev@gmail.com>
2022-01-06 22:06:30 +0100

    wrapper: just repeat the thing

--- plugin/src/main/kotlin/fr/stardustenterprises/gradle/rust/wrapper/task/SimpleTasks.kt
+++ plugin/src/main/kotlin/fr/stardustenterprises/gradle/rust/wrapper/task/SimpleTasks.kt
@@ -4,7 +4,7 @@ import fr.stardustenterprises.gradle.common.task.Task
 import fr.stardustenterprises.gradle.rust.wrapper.task.wrap.WrappedTask
 
 @Task(group = "rust", name = "test")
-class TestTask : WrappedTask(TestTask::class.java.getDeclaredAnnotation(Task::class.java).name)
+class TestTask : WrappedTask("test")
 
 @Task(group = "rust", name = "run")
-class RunTask : WrappedTask(RunTask::class.java.getDeclaredAnnotation(Task::class.java).name)
+class RunTask : WrappedTask("run")
