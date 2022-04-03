40accd776a493c861ee64a28a968ce784f89a5c0
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-03 13:58:40 +0200

    project: bumped to 3.2.1

--- buildSrc/src/main/kotlin/Coordinates.kt
+++ buildSrc/src/main/kotlin/Coordinates.kt
@@ -7,7 +7,7 @@ object Coordinates {
     const val REPO_ID = "stardust-enterprises/gradle-rust"
 
     const val GROUP = "fr.stardustenterprises.rust"
-    const val VERSION = "3.2.0"
+    const val VERSION = "3.2.1"
 }
 
 object Pom {
